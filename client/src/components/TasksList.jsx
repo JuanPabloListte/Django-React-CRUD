d436d6b914eca4a3164046b6fcedbe7af32dd50d
Juan Pablo Listte <juanpilistte@gmail.com>
2024-03-05 19:51:53 -0300

    finish the hole task project

---
+++ client/src/components/TasksList.jsx
@@ -0,0 +1,27 @@
+import {useEffect, useState} from 'react'
+import {getAllTask} from '../api/task.api'
+import {TaskCard} from './TaskCard'
+
+export function TasksList() {
+
+    const [tasks, setTasks] = useState([])
+
+    useEffect(() => {
+        
+        async function loadTasks() {
+            const res = await getAllTask()
+            setTasks(res.data)
+        }
+        loadTasks()
+    }, [])
+
+  return (
+    <div className='grid grid-cols-3 gap-3'>
+        {
+            tasks.map(task => (
+                <TaskCard key={task.id} task={task}/>
+            ))
+        }
+    </div>
+  )
+}
--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -128,7 +128,13 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:5174",
+    "http://127.0.0.1:5174"
+]
+
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
